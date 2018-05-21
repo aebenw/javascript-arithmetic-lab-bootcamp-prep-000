391e6cab5c50bb7c440e36fc660939494b56170e
Andrew Woodward <ebenwoodward@icloud.com>
2018-05-21 14:27:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
 
+
+
+it('add(a, b) adds two numbers and returns the result', function add(a,b) {
+  return a+b;
+}
+
+function subtract (a,b){
+  return a-b;
+}
+
+function 
 function makeInt(n) {
   return parseInt(n, 10);
 }
+
+
+it('subtract(a, b) subtracts b from a and returns the result', function() {
+  expect(subtract(a, b)).toEqual(a - b)
+})
+
+it('multiply(a, b) multiplies two numbers and returns the result', function() {
+  expect(multiply(a, b)).toEqual(a * b)
+})
+
+it('divide(a, b) divides a by b and returns the result', function() {
+  expect(divide(a, b)).toEqual(a / b)
+})
+
+it('inc(n) increments n and returns the result', function() {
+  expect(inc(a)).toEqual(a + 1)
+})
+
+it('dec(n) decrements n and returns the result', function() {
+  expect(dec(a)).toEqual(a - 1)
+})
+
 describe('makeInt(n)', function() {
   it('parses n as an integer and returns the parsed integer', function() {
     expect(makeInt(a.toString())).toEqual(a)
